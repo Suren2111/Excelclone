ad06c4ff825c7e03ab31e0ca933bd0573e1de692
Suren2111 <surendiran2111@gmail.com>
2022-05-07 13:34:29 +0530

    dropdown functionality done

--- menu.js
+++ menu.js
@@ -4,7 +4,8 @@ let underline = document.querySelector(".underline");
 let leftalign = document.querySelector(".left");
 let centeralign = document.querySelector(".center");
 let rightalign = document.querySelector(".right");
-
+let style = document.querySelector("#styles");
+let size=document.querySelector("#styles-size");
 
 bold.addEventListener("click", function () {
     setFontInUI("bold", bold);
@@ -72,38 +73,55 @@ centeralign.addEventListener("click", function () {
     setAlignUI("center", centeralign);
 })
 
-function setAlignUI(stylename,element) {
-   if(lastselectedcell){
-       let {rowid,colid}=getrowIdcolIdFromElement(lastselectedcell);
-       let cellobj=db[rowid][colid];
-       if(cellobj.alignstyle[stylename]){
-        if(stylename=='left'){
-            lastselectedcell.style.textAlign = "none";
-          }
-          else if(stylename=='center'){
-           lastselectedcell.style.textAlign = "left";
-          }
-          else{
-           lastselectedcell.style.textAlign = "left";
-          }
-          element.classList.remove("font-style-active")
+function setAlignUI(stylename, element) {
+    if (lastselectedcell) {
+        let { rowid, colid } = getrowIdcolIdFromElement(lastselectedcell);
+        let cellobj = db[rowid][colid];
+        if (cellobj.alignstyle[stylename]) {
+            if (stylename == 'left') {
+                lastselectedcell.style.textAlign = "none";
+            }
+            else if (stylename == 'center') {
+                lastselectedcell.style.textAlign = "left";
+            }
+            else {
+                lastselectedcell.style.textAlign = "left";
+            }
+            element.classList.remove("font-style-active")
 
-       }
-       else{
-           if(stylename=='left'){
-             lastselectedcell.style.textAlign = "left";
-           }
-           else if(stylename=='center'){
-            lastselectedcell.style.textAlign = "center";
-           }
-           else{
-            lastselectedcell.style.textAlign = "right";
-           }
-           element.classList.add("font-style-active")
-       }
-       cellobj.alignstyle[stylename]=!cellobj.alignstyle[stylename]
-   }
+        }
+        else {
+            if (stylename == 'left') {
+                lastselectedcell.style.textAlign = "left";
+            }
+            else if (stylename == 'center') {
+                lastselectedcell.style.textAlign = "center";
+            }
+            else {
+                lastselectedcell.style.textAlign = "right";
+            }
+            element.classList.add("font-style-active")
+        }
+        cellobj.alignstyle[stylename] = !cellobj.alignstyle[stylename]
+    }
 }
+
+style.addEventListener("change", function () {
+    let op = style.value;
+    if (lastselectedcell) {
+       lastselectedcell.style.fontFamily = op;
+    }
+})
+size.addEventListener("change",function(){
+    let sz=size.value;
+    if(lastselectedcell){
+        lastselectedcell.style.fontSize=sz+"px";
+    }
+})
+
+
+
+
 
 
 
